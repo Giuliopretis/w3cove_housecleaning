55988ea03d38f6ca549ed80ea4e74a446aa3475f
Giulio Pretis <giuliopretis@gmail.com>
2023-02-12 18:46:12 +0100

    added right configs for firestore

--- lib/data/api_provider.dart
+++ lib/data/api_provider.dart
@@ -1,7 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:w3cove_housecleaning/data/models/operation.dart';
+import 'package:w3cove_housecleaning/globals.dart';
 
 class ApiProvider {
-  ApiProvider() {
-    var db = FirebaseFirestore.instance;
+  static var db = FirebaseFirestore.instance;
+
+  void getOperationsList() async {
+    // Future<List<Operation>> getOperationsList() async {
+    var users = await db.collection(userCollection).get();
+    print(users);
   }
 }
--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:w3cove_housecleaning/data/api_provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +9,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final ApiProvider _apiProvider = ApiProvider();
+
+  @override
+  void initState() {
+    super.initState();
+    _apiProvider.getOperationsList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
